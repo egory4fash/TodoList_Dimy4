0c41945a4afaecd2918398e490f230ad11734c5d
egory4fash <egormaksul@gmail.com>
2021-11-23 18:49:25 +0300

    1st

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoList/>
+    </div>
+  );
+}
+
+const TodoList = () => {
+  return (
+      <div>
+        ToDoList
+      </div>
+  )
+}
+
+export default App;
