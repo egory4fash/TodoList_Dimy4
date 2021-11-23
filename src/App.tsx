f342a17c5f138c0eeb40f5cf541188ad174fbf62
egory4fash <egormaksul@gmail.com>
2021-11-23 21:01:22 +0300

    Dimych TodoList Lesson1

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,33 @@
 import React from 'react';
 import './App.css';
+import {TasksType, TodoList} from "./TodoList";
+
+
+const tasks1: Array<TasksType> = [
+    {id: 1, title: 'HTML', isDone: true},
+    {id: 2, title: 'CSS', isDone: true},
+    {id: 3, title: 'React', isDone: false}
+]
+
+const tasks2: Array<TasksType> = [
+    {id: 1, title: 'It', isDone: true},
+    {id: 2, title: 'Venom', isDone: true},
+    {id: 3, title: 'Mist', isDone: false}
+]
 
 function App() {
-  return (
-    <div className="App">
-      <TodoList/>
-    </div>
-  );
-}
 
-const TodoList = () => {
-  return (
-      <div>
-        ToDoList
-      </div>
-  )
+
+    return (
+        <div className="App">
+            <TodoList
+                title='What to learn'
+                tasks={tasks1}/>
+            <TodoList
+                title='Movies'
+                tasks={tasks2}/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/TodoList.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+
+export type TasksType = {
+    id:number
+    title:string
+    isDone:boolean
+}
+type TodoListPropsType = {
+    title: string
+    tasks:Array<TasksType>
+}
+
+export const TodoList = (props:TodoListPropsType) => {
+    return (
+        <div>
+            <h3>
+                {props.title}
+                <div>
+                    <input/>
+                    <button>+</button>
+                </div>
+                <ul>
+                    <li><input type="checkbox" checked={props.tasks[0].isDone}/>
+                        <span>{props.tasks[0].title}</span>
+                    </li>
+                    <li><input type="checkbox" checked={props.tasks[1].isDone}/>
+                        <span>{props.tasks[1].title}</span>
+                    </li>
+                    <li><input type="checkbox" checked={props.tasks[2].isDone}/>
+                        <span>{props.tasks[2].title}</span>
+                    </li>
+                </ul>
+                <div>
+                    <button>All</button>
+                    <button>Active</button>
+                    <button>Completed</button>
+                </div>
+            </h3>
+        </div>
+    )
+}
